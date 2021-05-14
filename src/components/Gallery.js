45a9dcd197c192c16b15035a2c7529227ede51d3
Deepan Jeganathan <Deepan.Jeganathan@belden.com>
2021-05-14 10:58:26 -0400

    add styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,27 +18,44 @@ fetch_data()
     dispatch(fetch_art());
   }
 
+  function OpenModal(e){
+
+ e.target.className==="text-primary"? e.target.className="text-success": e.target.className="text-primary";
+
+  }
+
+  // function ShowPlusIcon(e){
+  //   document.getElementById(e).style.backgroundColor='gray';
+    
+  // }
+  // function RemovePlusIcon(e){
+  // document.getElementById(e).style.backgroundColor='';
+   
+  // }
+
+  
 
 
 //  const gallery="hello";
 
   const gallery = results.data.map((x) => (
-    <div className="col-md-4 col-sm-6">
-      <Card className="m-3">
-        <Card.Img variant="top" src={x.imageURL} />
-        <Card.Body>
-        
-          <Card.Title>{x.title}</Card.Title>
+    <div  className="col-md-6 col-sm-12 col-xl-4">
+      <Card id={x.artModelId} onMouseOver={ShowPlusIcon} onMouseOut={RemovePlusIcon} onClick={OpenModal} className="m-3" >
+        {/* <Card.Img variant="top" src={x.imageURL} /> */}
+        <Card.Body>        
+          <Card.Title >{x.title}</Card.Title>
           <Card.Text>
-           Created by {x.artist} and installed  on {x.yeaR_INSTALLED}.
+           Created by {x.artist} and installed on {x.yeaR_INSTALLED}.
+           {x.description}
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <small className="text-muted">{x.location}</small>
+          <small className="text-muted">Located at {x.location} in {x.warD_FULLNAME}</small>
         </Card.Footer>
       </Card>
     </div>
   ));
+  
 if (results.loading==false) {
     return <Row>{gallery}</Row>;
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -23,14 +23,7 @@ function Main() {
         </Container>
       </Jumbotron>
       <Row>
-        <div className="col-6 offset-3">
-          <ListGroup horizontal>
-            <ListGroup.Item>This</ListGroup.Item>
-            <ListGroup.Item>ListGroup</ListGroup.Item>
-            <ListGroup.Item>renders</ListGroup.Item>
-            <ListGroup.Item>horizontally!</ListGroup.Item>
-          </ListGroup>
-        </div>
+        
       </Row>
     </>
   );
